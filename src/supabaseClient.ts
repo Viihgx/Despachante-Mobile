8fd4e83bc0d9db271ee66e8a1d2bb3c982d3ea66
Viihgx <vitoriasamara.g77@gmail.com>
2024-08-18 19:02:12 -0300

    :sparkles: adiciona config e integração com supabase

---
+++ src/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://zlzoirfwdhmwmesytvzl.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpsem9pcmZ3ZGhtd21lc3l0dnpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MzQxOTYsImV4cCI6MjAyNjExMDE5Nn0.QxDTeHLAzf_Re5gIdGo277zutfvxLyamc7xGemWzZ3M';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
