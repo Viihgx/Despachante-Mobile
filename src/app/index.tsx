15a4c7c28aba4994829e698577be01925619ff6d
Viihgx <vitoriasamara.g77@gmail.com>
2024-09-18 14:42:41 -0300

    :sparkles: Adiciona popup de detalhes dos servicos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Alert, Linking } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Alert, Linking, Modal, Button, Animated } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import axios from 'axios';
 import { Link, useRouter } from 'expo-router';
-import { MaterialIcons } from '@expo/vector-icons';  // Ícone de menu
-import { Menu, MenuItem } from 'react-native-material-menu'; 
+import { MaterialIcons } from '@expo/vector-icons';  
+import { Menu, MenuItem } from 'react-native-material-menu';
 
 interface Servico {
   tipo_servico: string;
   forma_pagamento: string;
   status_servico: string;
   data_solicitacao: string;
-  file_pdfs: string[]; 
+  file_pdfs: string[];
+  nome_completo?: string;
+  placa_do_veiculo?: string;
 }
 
 export default function HomeScreen() {
   const [userName, setUserName] = useState<string>('');
   const [servicos, setServicos] = useState<Servico[]>([]);
   const [menuVisible, setMenuVisible] = useState(false); // Controla a visibilidade do menu
-  const API_URL = 'http://192.168.18.20:5000';  // Substitua pela URL do seu backend
+  const [modalVisible, setModalVisible] = useState(false); // Controla a visibilidade do modal
+  const [selectedServico, setSelectedServico] = useState<Servico | null>(null); // Armazena o serviço selecionado
+  const [fadeAnim] = useState(new Animated.Value(0)); // Para animação do modal
+  const API_URL = 'http://192.168.18.20:5000'; // Substitua pela URL do seu backend
   const router = useRouter();
 
   useEffect(() => {
@@ -33,29 +38,24 @@ export default function HomeScreen() {
 
         const userResponse = await axios.get(`${API_URL}/api/user-data`, {
           headers: {
-            'Authorization': `Bearer ${token}`
-          }
+            Authorization: `Bearer ${token}`,
+          },
         });
 
         setUserName(userResponse.data.name);
 
         const servicosResponse = await axios.get(`${API_URL}/api/meus-servicos`, {
           headers: {
-            'Authorization': `Bearer ${token}`
-          }
+            Authorization: `Bearer ${token}`,
+          },
         });
 
-        // console.log('Dados dos serviços:', servicosResponse.data.servicos);
-
-        // Certifica que file_pdfs seja sempre um array
         const servicosComPdfs = servicosResponse.data.servicos.map((servico: any) => {
           let file_pdfs = servico.file_pdfs;
 
           if (typeof file_pdfs === 'string') {
-            // Se file_pdfs for uma string, converta para um array
             file_pdfs = [file_pdfs];
           } else if (!Array.isArray(file_pdfs)) {
-            // Se não for array, defina como um array vazio
             file_pdfs = [];
           }
 
@@ -66,7 +66,6 @@ export default function HomeScreen() {
         });
 
         setServicos(servicosComPdfs);
-
       } catch (error) {
         console.error('Erro ao buscar dados do usuário:', error);
         Alert.alert('Erro', 'Não foi possível carregar os dados do usuário');
@@ -89,9 +88,29 @@ export default function HomeScreen() {
     Linking.openURL(url);
   };
 
+  const openModal = (servico: Servico) => {
+    setSelectedServico(servico);
+    setModalVisible(true);
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const closeModal = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => {
+      setModalVisible(false);
+      setSelectedServico(null);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      {/* Ícone de menu no canto superior direito */}
       <View style={styles.menuContainer}>
         <Menu
           visible={menuVisible}
@@ -122,26 +141,16 @@ export default function HomeScreen() {
         <ScrollView contentContainerStyle={styles.servicosContainer}>
           {servicos.length > 0 ? (
             servicos.map((servico, index) => (
-              <View key={index} style={styles.servicoCard}>
-                <Text style={styles.servicoText}>Tipo de Serviço: {servico.tipo_servico}</Text>
-                <Text style={styles.servicoText}>Forma de Pagamento: {servico.forma_pagamento}</Text>
-                <Text style={styles.servicoText}>Status: {servico.status_servico}</Text>
-                <Text style={styles.servicoText}>Data da Solicitação: {new Date(servico.data_solicitacao).toLocaleDateString()}</Text>
-                
-                {servico.file_pdfs.length > 0 && (
-                  <View>
-                    {servico.file_pdfs.map((fileUrl, fileIndex) => (
-                      <TouchableOpacity
-                        key={fileIndex}
-                        style={styles.downloadButton}
-                        onPress={() => handleDownload(fileUrl)}
-                      >
-                        <Text style={styles.downloadText}>Baixar PDF {fileIndex + 1}</Text>
-                      </TouchableOpacity>
-                    ))}
-                  </View>
-                )}
-              </View>
+              <TouchableOpacity key={index} onPress={() => openModal(servico)}>
+                <View style={styles.servicoCard}>
+                  <Text style={styles.servicoText}>Tipo de Serviço: {servico.tipo_servico}</Text>
+                  <Text style={styles.servicoText}>Forma de Pagamento: {servico.forma_pagamento}</Text>
+                  <Text style={styles.servicoText}>Status: {servico.status_servico}</Text>
+                  <Text style={styles.servicoText}>
+                    Data da Solicitação: {new Date(servico.data_solicitacao).toLocaleDateString()}
+                  </Text>
+                </View>
+              </TouchableOpacity>
             ))
           ) : (
             <Text style={styles.noServicosText}>Você ainda não solicitou nenhum serviço.</Text>
@@ -149,7 +158,46 @@ export default function HomeScreen() {
         </ScrollView>
       </View>
 
-      <Link href="/login" style={styles.profileLink}>Ir para o Login</Link>
+      {/* Modal para exibir os detalhes do serviço */}
+      <Modal visible={modalVisible} transparent={true} animationType="fade">
+        <Animated.View style={[styles.modalContainer, { opacity: fadeAnim }]}>
+          {selectedServico && (
+            <View style={styles.modalContent}>
+              <Text style={styles.modalTitle}>Detalhes do Serviço</Text>
+              <Text style={styles.modalText}>Tipo de Serviço: {selectedServico.tipo_servico}</Text>
+              <Text style={styles.modalText}>Forma de Pagamento: {selectedServico.forma_pagamento}</Text>
+              <Text style={styles.modalText}>Status: {selectedServico.status_servico}</Text>
+              <Text style={styles.modalText}>
+                Data da Solicitação: {new Date(selectedServico.data_solicitacao).toLocaleDateString()}
+              </Text>
+              <Text style={styles.modalText}>Nome Completo: {selectedServico.nome_completo}</Text>
+              <Text style={styles.modalText}>Placa do Veículo: {selectedServico.placa_do_veiculo}</Text>
+
+              {selectedServico.file_pdfs.length > 0 && (
+                <View>
+                  {selectedServico.file_pdfs.map((fileUrl, fileIndex) => (
+                    <TouchableOpacity
+                      key={fileIndex}
+                      style={styles.downloadButton}
+                      onPress={() => handleDownload(fileUrl)}
+                    >
+                      <Text style={styles.downloadText}>Baixar PDF {fileIndex + 1}</Text>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+              )}
+
+              <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
+                <Text style={styles.closeButtonText}>Fechar</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Animated.View>
+      </Modal>
+
+      <Link href="/login" style={styles.profileLink}>
+        Ir para o Login
+      </Link>
     </View>
   );
 }
@@ -244,6 +292,35 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 10,
   },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fundo escuro translúcido
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 25,
+    margin: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
   downloadButton: {
     backgroundColor: '#007bff',
     padding: 10,
@@ -251,6 +328,18 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   downloadText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  closeButton: {
+    backgroundColor: '#ff4d4d',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  closeButtonText: {
     color: '#fff',
     fontSize: 16,
   },
