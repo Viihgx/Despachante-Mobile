b6bdd3b01102b3e6ad7604624c9a8e907b77df46
Viihgx <vitoriasamara.g77@gmail.com>
2024-11-19 17:21:34 -0300

    :bug: Ajusta erro para carregamento correto do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    // NOTE: Update this to include the paths to all of your component files.
-    mode: "jit",
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}", // Apenas arquivos necessários
-      "./app/**/*.{js,jsx,ts,tsx}", // Inclui arquivos da pasta app (Expo Router)
-    ],
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}"
+  ],
     presets: [require("nativewind/preset")],
     theme: {
       extend: {},
